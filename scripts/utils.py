5016446bb5414a01d3acff178bb8c756ed50c2da
iamaber <abirislam1892@gmail.com>
2025-07-29 19:25:29 +0600

    Implented get_model function to load a pre-trained model

--- scripts/utils.py
+++ scripts/utils.py
@@ -11,3 +11,20 @@ class Config:
         self.intermediate_features = 640
         self.num_classes = 2  
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+def get_model(model_path, num_classes):
+    config = Config()
+    model = models.mobilenet_v3_large(weights=None)
+    model.classifier = nn.Sequential(
+        nn.Linear(960, 1280),
+        nn.Hardswish(),
+        nn.Dropout(p=config.dropout_rate),
+        nn.Linear(1280, config.intermediate_features),
+        nn.Hardswish(),
+        nn.Dropout(p=config.dropout_rate),
+        nn.Linear(config.intermediate_features, num_classes),
+    )
+    model.load_state_dict(torch.load(model_path, map_location=torch.device(config.device)))
+    model.to(config.device)
+    model.eval()
+    return model
